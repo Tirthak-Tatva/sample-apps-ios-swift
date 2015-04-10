7ad8f4aa2d95e985736223ec7bdfd430184d4f9b
Justin R. Miller <incanus@codesorcery.net>
2015-04-10 12:17:12 -0700

    add KIF UI sample test

---
+++ BitriseSwiftSampleAppTests/SampleKIFTest.m
@@ -0,0 +1,16 @@
+#import <KIF/KIF.h>
+#import <UIKit/UIKit.h>
+
+@interface SampleKIFTest : KIFTestCase
+
+@end
+
+@implementation SampleKIFTest
+
+- (void)testButton {
+    [tester tapViewWithAccessibilityLabel:@"The test"];
+    [tester waitForTimeInterval:2.0];
+    [tester tapViewWithAccessibilityLabel:@"OK"];
+}
+
+@end
